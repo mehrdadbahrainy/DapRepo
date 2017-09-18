e73227ee1ca9a99f488f05e1e97f6fac80eaa9d9
Mehrdad Bahrainy <mehrdadbahrainy@gmail.com>
2017-09-19 01:13:32 +0430

    Create DapRepo.DataAccess project and generic repository class

---
+++ DapRepo.DataAccess/GenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace DapRepo.DataAccess
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly IDbConnection DbConnection;
+
+        public GenericRepository(IDbConnection dbConnection)
+        {
+            this.DbConnection = dbConnection;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IGenericRepository<T>.Insert(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(IEnumerable<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(IEnumerable<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(IEnumerable<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DapRepo.DataAccess/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DapRepo.DataAccess
+{
+    public interface IGenericRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        int Insert(T obj);
+        int Insert(IEnumerable<T> list);
+        bool Update(T obj);
+        bool Update(IEnumerable<T> list);
+        bool Delete(T obj);
+        bool Delete(IEnumerable<T> list);
+        bool DeleteAll();
+    }
+}
